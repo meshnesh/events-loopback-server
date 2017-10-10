1b6d0c86cd82e63190ac77d8e8eb79e1b117171a
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-10-10 19:43:46 +0300

    implemented the router, included materialize and made the UI

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
+import NavBar from './components/NavBar';
 
 const App = () => (
   <div>
-    <Main />
+    <NavBar />
+    <div className="container">
+      <Main />
+    </div>
+    <div className="fixed-action-btn">
+      <Link to="/meetups/add" className="btn-floating btn-large red">
+        <i className="fa fa-plus fa-3x"></i>
+      </Link>
+    </div>
   </div>
 )
 
---
+++ client_src/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="blue darken-4">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo center">EventZ</a>
+            <a data-activates="main-menu" className="button-collapse show-on-large">
+              <i className="fa fa-bars fa-2x"></i>
+            </a>
+            <ul className="right hide-on-small-only">
+              <li>
+                <Link to="/">
+                  <i className="fa fa-users"></i> Events
+                </Link>
+              </li>
+            </ul>
+            <ul className="side-nav" id="main-menu">
+              <li>
+                <Link to="/">
+                  <i className="fa fa-users fa-2x"></i> Events
+                </Link>
+              </li>
+              <li>
+                <Link to="/events/add">
+                  <i className="fa fa-plus fa-2x"></i> Add Event
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
